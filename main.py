0d81448fec903cd3de561987c3ca5853925492e8
adebola <adeboladesoyin@gmail.com>
2025-09-19 17:41:26 +0100

    committed all changes to git

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import requests
+import numpy as np
+
+url = "C:\Users\adebola.adesoyin\OneDrive - Avon Healthcare Ltd\Course+Resources\Desktop\Mine\Data Engineering\SCD 2 Project"
+
+data = pd.read_csv(url)
+data.head()
